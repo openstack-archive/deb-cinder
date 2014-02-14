eharney/cinder

GlusterFS: Use correct base argument when deleting attached snaps
When deleting the most recent snapshot, the 'file_to_merge' field
which translates into the base= field for libvirt's blockRebase
call in Nova must be set depending on whether other snapshots exist.

If there are no other snapshots, base = None, which results in
libvirt clearing the qcow2 backing file pointer for the active
disk image.

If there are other snapshots, pass the parent of the file being
deleted as the new base file.  The snapshot info pointer for the
prior base file must also be updated in this case.

Change-Id: If7bc8259b031d0406346caafb8f688e65a38dba6

cinder/tests/test_glusterfs.py
@@ -1139,7 +1139,7 @@ class GlusterFsDriverTestCase(test.TestCase):
                           snap_ref)
 
     def test_delete_snapshot_online_1(self):
-        """Delete the newest snapshot."""
+        """Delete the newest snapshot, with only one snap present."""
         (mox, drv) = self._mox, self._driver
 
         volume = self._simple_volume()
@@ -1189,7 +1189,17 @@ class GlusterFsDriverTestCase(test.TestCase):
         backing file: %s
         """ % (snap_file, volume_file)
         img_info = imageutils.QemuImgInfo(qemu_img_info_output)
+
+        vol_qemu_img_info_output = """image: %s
+        file format: raw
+        virtual size: 1.0G (1073741824 bytes)
+        disk size: 173K
+        """ % volume_file
+        volume_img_info = imageutils.QemuImgInfo(vol_qemu_img_info_output)
+
         image_utils.qemu_img_info(snap_path).AndReturn(img_info)
+
+        image_utils.qemu_img_info(volume_path).AndReturn(volume_img_info)
 
         drv._read_info_file(info_path, empty_if_missing=True).\
             AndReturn(snap_info)
@@ -1197,7 +1207,7 @@ class GlusterFsDriverTestCase(test.TestCase):
         delete_info = {
             'type': 'qcow2',
             'merge_target_file': None,
-            'file_to_merge': volume_file,
+            'file_to_merge': None,
             'volume_id': self.VOLUME_UUID
         }
 
@@ -1228,7 +1238,7 @@ class GlusterFsDriverTestCase(test.TestCase):
         drv.delete_snapshot(snap_ref)
 
     def test_delete_snapshot_online_2(self):
-        """Delete the middle snapshot."""
+        """Delete the middle of 3 snapshots."""
         (mox, drv) = self._mox, self._driver
 
         volume = self._simple_volume()
@@ -1282,7 +1292,16 @@ class GlusterFsDriverTestCase(test.TestCase):
         """ % (snap_file, volume_file)
         img_info = imageutils.QemuImgInfo(qemu_img_info_output)
 
+        vol_qemu_img_info_output = """image: %s
+        file format: raw
+        virtual size: 1.0G (1073741824 bytes)
+        disk size: 173K
+        """ % volume_file
+        volume_img_info = imageutils.QemuImgInfo(vol_qemu_img_info_output)
+
         image_utils.qemu_img_info(snap_path).AndReturn(img_info)
+
+        image_utils.qemu_img_info(volume_path).AndReturn(volume_img_info)
 
         drv._read_info_file(info_path, empty_if_missing=True).\
             AndReturn(snap_info)

openstack-archive/deb-cinder

Closes-Bug: #1262880
(cherry picked from commit 186221779a92002ff9fa13c254710c0abb3803be)
Conflicts:
	cinder/tests/test_glusterfs.py

cinder/tests/test_glusterfs.py
@@ -1221,7 +1221,7 @@ class GlusterFsDriverTestCase(test.TestCase):
                           snap_ref)
 
     def test_delete_snapshot_online_1(self):
-        """Delete the newest snapshot."""
+        """Delete the newest snapshot, with only one snap present."""
         (mox, drv) = self._mox, self._driver
 
         volume = self._simple_volume()
@@ -1272,7 +1272,17 @@ class GlusterFsDriverTestCase(test.TestCase):
         backing file: %s
         """ % (snap_file, volume_file)
         img_info = image_utils.QemuImgInfo(qemu_img_info_output)
+
+        vol_qemu_img_info_output = """image: %s
+        file format: raw
+        virtual size: 1.0G (1073741824 bytes)
+        disk size: 173K
+        """ % volume_file
+        volume_img_info = image_utils.QemuImgInfo(vol_qemu_img_info_output)
+
         image_utils.qemu_img_info(snap_path).AndReturn(img_info)
+
+        image_utils.qemu_img_info(volume_path).AndReturn(volume_img_info)
 
         drv._read_info_file(info_path, empty_if_missing=True).\
             AndReturn(snap_info)
@@ -1280,7 +1290,7 @@ class GlusterFsDriverTestCase(test.TestCase):
         delete_info = {
             'type': 'qcow2',
             'merge_target_file': None,
-            'file_to_merge': volume_file,
+            'file_to_merge': None,
             'volume_id': self.VOLUME_UUID
         }
 
@@ -1311,7 +1321,7 @@ class GlusterFsDriverTestCase(test.TestCase):
         drv.delete_snapshot(snap_ref)
 
     def test_delete_snapshot_online_2(self):
-        """Delete the middle snapshot."""
+        """Delete the middle of 3 snapshots."""
         (mox, drv) = self._mox, self._driver
 
         volume = self._simple_volume()
@@ -1366,7 +1376,16 @@ class GlusterFsDriverTestCase(test.TestCase):
         """ % (snap_file, volume_file)
         img_info = image_utils.QemuImgInfo(qemu_img_info_output)
 
+        vol_qemu_img_info_output = """image: %s
+        file format: raw
+        virtual size: 1.0G (1073741824 bytes)
+        disk size: 173K
+        """ % volume_file
+        volume_img_info = image_utils.QemuImgInfo(vol_qemu_img_info_output)
+
         image_utils.qemu_img_info(snap_path).AndReturn(img_info)
+
+        image_utils.qemu_img_info(volume_path).AndReturn(volume_img_info)
 
         drv._read_info_file(info_path, empty_if_missing=True).\
             AndReturn(snap_info)